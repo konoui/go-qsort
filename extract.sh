98fab59d6f91498b2fafe733b27a32ec5cddeddb
konoui <konoui.dev@gmail.com>
2023-01-27 12:18:13 +0900

    update

--- extract.sh
+++ extract.sh
@@ -5,6 +5,8 @@ sed -i.bak -E '/^\s+utilPrintf+/d' qsort.go
 sed -i.bak -E '/^\s+utilDump+/d' qsort.go
 ! grep -i import qsort.go
 ! grep -i util qsort.go
+! grep -i fmt qsort.go
+! grep -i import heapsort.go
 ! grep -i util heapsort.go
-
+! grep -i fmt heapsort.go
 echo success!
